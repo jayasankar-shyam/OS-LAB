7891874aa97659a691d1ac2bd62ad9e2a6794998
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2022-05-16 12:17:28 +0530

    Create display.sh

---
+++ display.sh
@@ -0,0 +1,16 @@
+echo "Enter your Name : "
+read name
+echo "Enter your Semester : "
+read sem
+echo "Enter your Batch : "
+read bat
+echo "Enter your Roll No : "
+read roll_no
+echo "Enter your Register No : "
+read reg_no
+echo ""
+echo "Name :$name"
+echo "Semester :$sem"
+echo "Batch :$bat"
+echo "Roll Number :$roll_no"
+echo "Register Number :$reg_no"
