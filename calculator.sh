71bb1ebb98ceeed24237145400e190e6e3bf7b0f
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2022-05-26 15:00:41 +0530

    Create calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,21 @@
+echo -n "Enter first number : "
+read a
+echo -n "Enter second number : "
+read b
+echo "Enter your choice "
+echo "1.Addition"
+echo "2.Subtraction"
+echo "3.Multiplication"
+echo "4.Division" 
+read choice
+case $choice in
+	1)res=$((a+b))
+	  ;;
+	2)res=$((a-b))
+	  ;;
+	3)res=$((a*b))
+	  ;;
+	4)res=$((a/b))
+	  ;;
+esac
+echo "Result : $res"
